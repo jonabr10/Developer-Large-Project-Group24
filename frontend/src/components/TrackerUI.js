4e011376ddc8333343e34cf3762baebbbfd774ab
Jon Abrigo <jonabrigo@knights.ucf.edu>
2021-07-03 17:38:56 -0400

    quick commit

--- frontend/src/components/TrackerUI.js
+++ frontend/src/components/TrackerUI.js
@@ -1,25 +1,21 @@
 import React, { useState } from 'react';
 
-function TrackerUI()
-{
+function TrackerUI() {
     const app_name = 'health-n-wellness'
-    function buildPath(route)
-    {
-        if (process.env.NODE_ENV === 'production')
-        {
-            return 'https://' + app_name +  '.herokuapp.com/' + route;
+    function buildPath(route) {
+        if (process.env.NODE_ENV === 'production') {
+            return 'https://' + app_name + '.herokuapp.com/' + route;
         }
-        else
-        {
+        else {
             return 'http://localhost:5000/' + route;
         }
     }
 
     var card = '';
     var search = '';
-    const [message,setMessage] = useState('');
-    const [searchResults,setResults] = useState('');
-    const [cardList,setCardList] = useState('');
+    const [message, setMessage] = useState('');
+    const [searchResults, setResults] = useState('');
+    const [cardList, setCardList] = useState('');
 
     var _ud = localStorage.getItem('user_data');
     var ud = JSON.parse(_ud);
@@ -27,61 +23,60 @@ function TrackerUI()
     var firstName = ud.firstName;
     var lastName = ud.lastName;
 
-    const addCard = async event =>
-    {
+    const addCard = async event => {
         event.preventDefault();
 
-        var obj = {userId:userId,card:card.value};
+        var obj = { userId: userId, card: card.value };
         var js = JSON.stringify(obj);
 
-        try
-        {
+        try {
             const response = await fetch(buildPath('/api/addcard'),
-            {method:'POST',body:js,headers:{'Content-Type':
-            'application/json'}});
+                {
+                    method: 'POST', body: js, headers: {
+                        'Content-Type':
+                            'application/json'
+                    }
+                });
 
             var txt = await response.text();
             var res = JSON.parse(txt);
 
-            if( res.error.length > 0 )
-            {
-                setMessage( "API Error:" + res.error );
+            if (res.error.length > 0) {
+                setMessage("API Error:" + res.error);
             }
-            else
-            {
+            else {
                 setMessage('Card has been added');
             }
         }
 
-        catch(e)
-        {
+        catch (e) {
             setMessage(e.toString());
         }
     };
 
-    const searchCard = async event =>
-    {
+    const searchCard = async event => {
         event.preventDefault();
 
-        var obj = {userId:userId,search:search.value};
+        var obj = { userId: userId, search: search.value };
         var js = JSON.stringify(obj);
 
-        try
-        {
+        try {
             const response = await fetch(buildPath('/api/searchcards'),
-            {method:'POST',body:js,headers:{'Content-Type':
-            'application/json'}});
+                {
+                    method: 'POST', body: js, headers: {
+                        'Content-Type':
+                            'application/json'
+                    }
+                });
 
             var txt = await response.text();
             var res = JSON.parse(txt);
             var _results = res.results;
             var resultText = '';
 
-            for( var i=0; i<_results.length; i++ )
-            {
+            for (var i = 0; i < _results.length; i++) {
                 resultText += _results[i];
-                if( i < _results.length - 1 )
-                {
+                if (i < _results.length - 1) {
                     resultText += ', ';
                 }
             }
@@ -90,29 +85,30 @@ function TrackerUI()
             setCardList(resultText);
         }
 
-        catch(e)
-        {
+        catch (e) {
             alert(e.toString());
             setResults(e.toString());
         }
     };
 
-    return(
+    return (
         <div id="cardUIDiv">
             <br />
             <input type="text" id="searchText" placeholder="Card To Search For"
-              ref={(c) => search = c} />
+                ref={(c) => search = c} />
             <button type="button" id="searchCardButton" class="buttons"
-              onClick={searchCard}> Search Card</button><br />
+                onClick={searchCard}> Search Card</button><br />
             <span id="cardSearchResult">{searchResults}</span>
             <p id="cardList">{cardList}</p><br /><br />
             <input type="text" id="cardText" placeholder="Card To Add"
-              ref={(c) => card = c} />
+                ref={(c) => card = c} />
             <button type="button" id="addCardButton" class="buttons"
-              onClick={addCard}> Add Card </button><br />
+                onClick={addCard}> Add Card </button><br />
             <span id="cardAddResult">{message}</span>
         </div>
     );
 }
+
+// sample comment
 
 export default TrackerUI;
